b3f1520e13eb45f0e583416c21cbdbb544cce2e6
Alberto Rodriguez <juniorrodriguez@Albertos-iMac.local>
2019-04-21 21:05:07 -0500

    deleated line to make code more readable

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,8 +20,7 @@ class TabLink {
     
       this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
     }
-    // <- Delete this comment block when you work on the if statement
-
+    
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
     
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
